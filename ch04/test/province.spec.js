cebb51893d75f738086a5881e0277941a42bbb7a
Lee, Jung Geun <cjred77@gmail.com>
2020-08-15 18:42:07 +0900

    ch04 4.4 fixture

--- ch04/test/province.spec.js
+++ ch04/test/province.spec.js
@@ -4,15 +4,17 @@ const { Province, sampleProvinceData } = require("../Province");
 const { assert } = require("chai");
 
 describe("province", function () {
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
   // 생산 부족분을 제대로 계산하는지
   it("shortfall", function () {
-    const asia = new Province(sampleProvinceData());
     assert.equal(asia.shortfall, 5);
   });
 
   // 초기 픽스처로부터 총수익이 제대로 계산되는지
   it("profit", function () {
-    const asia = new Province(sampleProvinceData());
     assert.equal(asia.profit, 230);
   });
 });
