b3311aea6b9cfc35b7cb2c665c257321a94f8798
이정근 <junggelee@ebay.com>
2020-08-22 11:47:00 +0900

    06 step 3

--- ch06/0601-ExtractFunction/after.js
+++ ch06/0601-ExtractFunction/after.js
@@ -1,15 +1,19 @@
 const Clock = require("../Clock");
 
 function printOwing(invoice) {
-  let outstanding = 0;
-
   printBanner();
+
   // 미해결 채무(outstanding)를 계산한다.
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+  const outstanding = calculateOutstanding(invoice);
 
   // 마감일(dueData)을 기록한다.
+  recordDueDate(invoice);
+
+  // 세부 사항을 출력한다.
+  printDetails(invoice, outstanding);
+}
+
+function recordDueDate(invoice) {
   const today = Clock.today;
 
   invoice.dueDate = new Date(
@@ -17,9 +21,14 @@ function printOwing(invoice) {
     today.getMonth(),
     today.getDate() + 30
   );
+}
 
-  // 세부 사항을 출력한다.
-  printDetails(invoice, outstanding);
+function calculateOutstanding(invoice) {
+  let result = 0;
+  for (const o of invoice.orders) {
+    result += o.amount;
+  }
+  return result;
 }
 
 function printBanner() {
