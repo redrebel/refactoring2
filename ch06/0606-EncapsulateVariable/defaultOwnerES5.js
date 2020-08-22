6219a756ff45c61f8d660e172a26abff3e414509
이정근 <junggelee@ebay.com>
2020-08-23 07:02:52 +0900

    6.6 step4

--- ch06/0606-EncapsulateVariable/defaultOwnerES5.js
+++ ch06/0606-EncapsulateVariable/defaultOwnerES5.js
@@ -3,9 +3,23 @@
 let defaultOwnerData = { firstName: "마틴", lastName: "파울러" };
 
 module.exports.defaultOwner = function defaultOwner() {
-  return Object.assign({}, defaultOwnerData);
+  return new Person(defaultOwnerData);
 };
 
 module.exports.setDefaultOwner = function setDefaultOwner(arg) {
   defaultOwnerData = arg;
 };
+
+class Person {
+  constructor(data) {
+    this._lastName = data.lastName;
+    this._firstName = data.firstName;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+  get firstName() {
+    return this._firstName;
+  }
+}
--- ch06/0606-EncapsulateVariable/test/test.spec.js
+++ ch06/0606-EncapsulateVariable/test/test.spec.js
@@ -6,4 +6,4 @@ assert.equal("파울러", owner1.lastName, "처음 값 확인");
 
 const owner2 = defaultOwner.defaultOwner();
 owner2.lastName = "파슨스";
-assert.equal("파슨스", owner1.lastName, "owner2를 변경한 후");
+assert.equal("파슨스", owner2.lastName, "owner2를 변경한 후");
