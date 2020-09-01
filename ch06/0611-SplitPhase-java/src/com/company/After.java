a711d8e2af65ada3f6848b78a5d648eab55fd3a8
이정근 <junggelee@ebay.com>
2020-09-01 09:32:32 +0900

    ch0611 Split Phase (java) step1

---
+++ ch06/0611-SplitPhase-java/src/com/company/After.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class After {
+
+    public static void main(String[] args) {
+        try{
+            System.out.println(run(args));;
+        } catch (Exception e) {
+            System.err.println(e);
+            System.exit(1);
+        }
+    }
+
+    private static long run(String[] args) throws java.io.IOException {
+        if(args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
+        CommandLine commandLine = new CommandLine();
+        commandLine.filename = args[args.length -1];
+        commandLine.onlyCountReady = Stream.of(args).anyMatch(arg -> "-r".equals(arg));
+        return countOrders(commandLine);
+    }
+
+    private static long countOrders(CommandLine commandLine) throws java.io.IOException {
+        File input = Paths.get(commandLine.filename).toFile();
+        ObjectMapper mapper = new ObjectMapper();
+        Order[] orders = mapper.readValue(input, Order[].class) ;
+
+        if(commandLine.onlyCountReady)
+            return Stream.of(orders)
+            .filter(o -> "ready".equals(o.status))
+            .count();
+        else
+            return orders.length;
+    }
+
+    static class CommandLine{
+        boolean onlyCountReady;
+        String filename;
+    }
+}
