8aea7ed5068bdae1cc197c44dff1aec17b743934
이정근 <cjred77@gmail.com>
2020-09-17 06:39:28 +0900

    ch08 02

---
+++ ch08/0802-MoveField/after1.js
@@ -0,0 +1,35 @@
+class Customer {
+  constructor(name, discountRate) {
+    this._name = name;
+    this._discountRate = discountRate;
+    this._contract = new CustomerContract(dateToday());
+  }
+
+  get discountRate() {
+    return this._discountRate;
+  }
+
+  _setDiscountRate(aNumber) {
+    this._discountRate = aNumber;
+  }
+
+  becomePreferred() {
+    this._setDiscountRate((this._discountRate += 0.03));
+  }
+  applyDiscount(amount) {
+    //return amount.subtract(amount.multiply(this._discountRate));
+    return amount - amount * this._discountRate;
+  }
+}
+
+class CustomerContract {
+  constructor(startDate) {
+    this._startDate = startDate;
+  }
+}
+
+function dateToday() {
+  return new Date();
+}
+
+module.exports = Customer;
