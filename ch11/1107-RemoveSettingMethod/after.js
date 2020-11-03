e6251bbcff2ec970a64a007c1ed6f99afa05c771
이정근 <cjred77@gmail.com>
2020-11-04 06:33:37 +0900

    ch1107

---
+++ ch11/1107-RemoveSettingMethod/after.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor(id) {
+    this._id;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(arg) {
+    this._name = arg;
+  }
+  get id() {
+    return this._id;
+  }
+}
---
+++ ch11/1107-RemoveSettingMethod/before.js
@@ -0,0 +1,14 @@
+class Person {
+  get name() {
+    return this._name;
+  }
+  set name(arg) {
+    this._name = arg;
+  }
+  get id() {
+    return this._id;
+  }
+  set id(arg) {
+    return (this._id = arg);
+  }
+}
