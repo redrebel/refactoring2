2a21f713d91933649e1e25f4ffd3be0a03a8d149
이정근 <cjred77@gmail.com>
2020-11-16 08:08:48 +0900

    ch1206

--- ch12/1206-ReplaceTypeCodeWithSubclasses/Employee2/after1.js
+++ ch12/1206-ReplaceTypeCodeWithSubclasses/Employee2/after1.js
@@ -1,17 +1,8 @@
-class EmployeeType {
-  constructor(aString) {
-    this._value = aString;
-  }
-  toString() {
-    return this._value;
-  }
-}
-
 class Employee {
   constructor(name, type) {
     this.validateType(type);
     this._name = name;
-    this._type = type;
+    this.type = type;
   }
 
   validateType(arg) {
@@ -27,19 +18,51 @@ class Employee {
     return this._type;
   }
   set type(arg) {
-    this._type = arg;
+    this._type = Employee.createEmployeeType(arg);
   }
 
-  get capitalizedType() {
-    return (
-      this.typeString.charAt(0).toUpperCase() +
-      this.typeString.substr(1).toLowerCase()
-    );
+  static createEmployeeType(aString) {
+    switch (aString) {
+      case "engineer":
+        return new Engineer();
+      case "manager":
+        return new Manager();
+      case "salesperson":
+        return new Salesperson();
+      default:
+        throw new Error(`{aString}라는 직원 유형은 없습니다.`);
+    }
   }
 
   toString() {
-    return `${this._name} (${this.capitalizedType})`;
+    return `${this._name} (${this.type.capitalizedName})`;
   }
 }
 
+class EmployeeType {
+  get capitalizedName() {
+    return (
+      this.toString().charAt(0).toUpperCase() +
+      this.toString().substr(1).toLowerCase()
+    );
+  }
+}
+
+class Engineer extends EmployeeType {
+  toString() {
+    return "engineer";
+  }
+}
+
+class Manager extends EmployeeType {
+  toString() {
+    return "manager";
+  }
+}
+
+class Salesperson extends EmployeeType {
+  toString() {
+    return "salesperson";
+  }
+}
 module.exports = Employee;
