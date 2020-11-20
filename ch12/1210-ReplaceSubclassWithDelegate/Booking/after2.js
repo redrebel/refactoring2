c9e4338451f8440abc543706136c6c506f8aef18
이정근 <cjred77@gmail.com>
2020-11-21 08:34:04 +0900

    ch1210

---
+++ ch12/1210-ReplaceSubclassWithDelegate/Booking/after2.js
@@ -0,0 +1,63 @@
+class Booking {
+  constructor(show, date) {
+    this._show = show;
+    this._date = date;
+    this.isPeakDay = true;
+  }
+
+  get hasTalkback() {
+    return this._premiumDelegate
+      ? this._premiumDelegate.hasTalkback
+      : this._show.hasOwnProperty("talkback") && !this.isPeakDay;
+  }
+
+  get basePrice() {
+    let result = this._show.price;
+    if (this.isPeakDay) result += Math.round(result * 0.15);
+    return this._premiumDelegate
+      ? this._premiumDelegate.extendBasePrice(result)
+      : result;
+  }
+
+  _bePremium(extras) {
+    this._premiumDelegate = new PremiumBookingDelegate(this, extras);
+  }
+}
+
+class PremiumBooking extends Booking {
+  constructor(show, date, extras) {
+    super(show, date);
+    this._extras = extras;
+  }
+
+  get hasDinner() {
+    return this._extras.hasOwnProperty("dinner") && !this.isPeakDay;
+  }
+}
+
+class PremiumBookingDelegate {
+  constructor(hostBooking, extras) {
+    this._host = hostBooking;
+    this._extras = extras;
+  }
+
+  get hasTalkback() {
+    return this._host._show.hasOwnProperty("talkback");
+  }
+
+  extendBasePrice(base) {
+    return Math.round(base + this._extras.premiumFee);
+  }
+}
+
+function createBooking(show, date) {
+  return new Booking(show, date);
+}
+
+function createPremiumBooking(show, date, extras) {
+  const result = new PremiumBooking(show, date, extras);
+  result._bePremium(extras);
+  return result;
+}
+
+module.exports = { createBooking, createPremiumBooking };
---
+++ ch12/1210-ReplaceSubclassWithDelegate/test/booking.spec.js
@@ -0,0 +1,60 @@
+const { describe } = require("mocha");
+const { expect } = require("chai");
+
+const show = {
+  price: 100,
+  hasOwnProperty: function () {
+    return true;
+  },
+};
+
+const date = new Date();
+const extra = { premiumFee: 20 };
+
+describe("1210 Replace Subclass with Delegate - before", () => {
+  const { Booking, PremiumBooking } = require("../Booking/before");
+  const booking = new Booking(show, date);
+  const premiumBooking = new PremiumBooking(show, date, extra);
+  it("Booking", () => {
+    expect(booking.hasTalkback).equals(false);
+    expect(booking.basePrice).equals(115);
+  });
+
+  it("PremiumBooking", () => {
+    expect(premiumBooking.hasTalkback).equals(true);
+    expect(premiumBooking.basePrice).equals(135);
+    expect(premiumBooking.hasDinner).equals(false);
+  });
+});
+
+describe("1210 Replace Subclass with Delegate - after1", () => {
+  const { createBooking, createPremiumBooking } = require("../Booking/after1");
+  const booking = createBooking(show, date);
+  const premiumBooking = createPremiumBooking(show, date, extra);
+  it("Booking", () => {
+    expect(booking.hasTalkback).equals(false);
+    expect(booking.basePrice).equals(115);
+  });
+
+  it("PremiumBooking", () => {
+    expect(premiumBooking.hasTalkback).equals(true);
+    expect(premiumBooking.basePrice).equals(135);
+    expect(premiumBooking.hasDinner).equals(false);
+  });
+});
+
+describe("1210 Replace Subclass with Delegate - after2", () => {
+  const { createBooking, createPremiumBooking } = require("../Booking/after2");
+  const booking = createBooking(show, date);
+  const premiumBooking = createPremiumBooking(show, date, extra);
+  it("Booking", () => {
+    expect(booking.hasTalkback).equals(false);
+    expect(booking.basePrice).equals(115);
+  });
+
+  it("PremiumBooking", () => {
+    expect(premiumBooking.hasTalkback).equals(true);
+    expect(premiumBooking.basePrice).equals(135);
+    expect(premiumBooking.hasDinner).equals(false);
+  });
+});
